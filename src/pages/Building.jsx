15d8409fb179fdd100fc4d5e253d8eab8305a112
Samantha Josette <acandidhart@gmail.com>
2025-07-28 22:31:54 -0400

    Reduce spacing between hero section and services section on Building page

--- src/pages/Building.jsx
+++ src/pages/Building.jsx
@@ -165,7 +165,7 @@ const Building = () => {
       </section>
 
       {/* Services Section */}
-      <section className="py-16 lg:py-24">
+      <section className="py-8 lg:py-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="space-y-16">
             {services.map((service, index) => {
