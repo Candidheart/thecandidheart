daf2a3573c49d7bf4e8262dee4d7de2e4413b94e
Samantha Josette <acandidhart@gmail.com>
2025-07-28 22:57:34 -0400

    Add /schedule route as alternative to /book-call for BookCall page

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/book-call" element={<BookCall />} />
+            <Route path="/schedule" element={<BookCall />} />
           </Routes>
         </main>
         <Footer />
